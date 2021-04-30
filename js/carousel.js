a10d2670c4d7a2f843726d9386f2af05a0b74074
daniyal_741 <daniyalanwaar741@gmail.com>
2021-05-01 01:44:31 +0500

    Drone Models

--- js/carousel.js
+++ js/carousel.js
@@ -10,4 +10,36 @@ $('.slider').slick({
     arrows:true,
     
   });
+
+  $('.models').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay:false,
+    arrows:true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+    
+  });
 });
+
