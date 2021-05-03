af3ed414a4f1a136948ce71e482d04b9dfa72ebc
daniyal_741 <daniyalanwaar741@gmail.com>
2021-05-04 04:11:49 +0500

    Pop-Ups

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 
-    $('.model').on('change', 'input:radio',function(){
+    $('.featured').on('change', 'input:radio',function(){
 
         if($(this).attr('id')==='purple')
         {
@@ -40,6 +40,47 @@ $(document).ready(function(){
            attr('src','./images/kisspng-mavic-pro-osmo-phantom-dji-unmanned-aerial-vehicle-drones-5ad46222eaa530.6921655215238681949611.png');
         }
      
-    })
+    });
+
+    $('.featured').on('click', '.model #eye', function(){
+
+
+
+      var pop_up = $(this).parents('.featured').children('.pop-up');
+      pop_up.find('.left-content').empty();
+      // pop_up.find('.right-content').empty();
+
+    
+
+      var res = $(this).parents('.model');
+      var clone1 = res.clone();
+     
+      clone1.removeClass();
+      clone1.addClass('model');
+      clone1.find('.options').remove();
+      clone1.find('.price').remove();
+      clone1.css('width','100%');
+      pop_up.find('.left-content').append(clone1); // pop_up = left-content
+
+
+      pop_up.css({
+
+         display: 'flex'
+         
+     });  // used to add multiple css attributes.
+
+
+    });
+
+    $('.pop-up').on('click', '.cross' , function(){
+
+      
+      var res = $(this).parent().parent(); // returns the .pop-up object
+      res.css('display','none');
+      res = $('input[name=eye]');
+      res.prop('checked', false);
+
+
+    });
 
 });
